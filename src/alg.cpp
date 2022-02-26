ff46e6792630eb1b3d7c87c9c8549c1482bab437
Аксенов Роман <98100948+raxenov0@users.noreply.github.com>
2022-02-26 23:15:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,34 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int first = 0;
+int last = size - 1;
+int index = -1;
+int mid;
+while (first <= last) {
+  mid = (first + last) / 2;
+  if (arr[mid] == value) {
+    index = mid;
+    break;
+    } else if (arr[mid] < value) {
+      first = mid+1;
+    } else if (arr[mid] > value) {
+      last = mid - 1;
+    }
+}	
+  if (index != -1) {
+    int start = index;
+    int count = 0;
+    while (arr[index] == value) {
+      count++;
+      index--;
+    }
+    index = start + 1;
+    while (arr[index] == value) {
+      count++;
+      index++;
+    }
+    return count;
+  }
+  return 0;
 }
