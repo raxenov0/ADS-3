e41faafb8882f4e4c051ed3670356906b0dcaa1d
Аксенов Роман <98100948+raxenov0@users.noreply.github.com>
2022-02-27 13:07:02 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,16 +5,16 @@ int first = 0;
 int last = size - 1;
 int index = -1;
 int mid;
-while (first <= last) {
-  mid = (first + last) / 2;
-  if (arr[mid] == value) {
-    index = mid;
-    } else if (arr[mid] < value) {
-      first = mid+1;
-    } else if (arr[mid] > value) {
-      last = mid - 1;
-    }
-}	
+  while (first <= last) {
+    mid = (first + last) / 2;
+    if (arr[mid] == value) {
+      index = mid;
+      } else if (arr[mid] < value) {
+        first = mid+1;
+      } else if (arr[mid] > value) {
+        last = mid - 1;
+      }
+  }	
   if (index != -1) {
     int start = index;
     int count = 0;
